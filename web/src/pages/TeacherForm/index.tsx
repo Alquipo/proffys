64e75ba1f2c2c56107ec045d99f3256b3b4c1b75
Alquipo <alquiponeto@outlook.com.br>
2020-08-05 20:29:53 -0300

    chore: styles teacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,6 +2,11 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 
+import "./styles.css";
+import Input from "../../components/Input";
+
+import warningIcon from "../../assets/images/icons/warning.svg";
+
 const TeacherForm = () => {
   return (
     <div id="page-teacher-form" className="container">
@@ -10,7 +15,31 @@ const TeacherForm = () => {
         description="O primeiro passo é preencher esse formulário de inscrição"
       />
 
-      <main></main>
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <Input name="name" label="Nome Completo" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a aula</legend>
+
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso Importante" />
+            Importante! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar cadastro</button>
+        </footer>
+      </main>
     </div>
   );
 };
