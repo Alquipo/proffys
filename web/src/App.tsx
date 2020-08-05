0903df67c481732b1481e04ee4bcb00cff64fe14
Alquipo <alquiponeto@outlook.com.br>
2020-08-05 11:36:30 -0300

    chore: global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import './assets/styles/global.css'
+
 function App() {
   return (
     <div className="App">
       <h1>teste</h1>
+      <button>asda</button>
     </div>
   );
 }
