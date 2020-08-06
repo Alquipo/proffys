358509c14c989047befa3939edabccfeb207f476
Alquipo <alquiponeto@outlook.com.br>
2020-08-05 21:05:47 -0300

    feat: Select component, textarea component

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 import PageHeader from "../../components/PageHeader";
 
-import "./styles.css";
 import Input from "../../components/Input";
+import Textarea from "../../components/Textarea";
+import Select from "../../components/Select";
 
 import warningIcon from "../../assets/images/icons/warning.svg";
+
+import "./styles.css";
 
 const TeacherForm = () => {
   return (
@@ -22,13 +25,56 @@ const TeacherForm = () => {
           <Input name="name" label="Nome Completo" />
           <Input name="avatar" label="Avatar" />
           <Input name="whatsapp" label="Whatsapp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
 
-          <Input name="subject" label="Matéria" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Física", label: "Física" },
+              { value: "Química", label: "Química" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "Historia", label: "Historia" },
+              { value: "Matemática", label: "Geografia" },
+              { value: "Português", label: "Geografia" },
+            ]}
+          />
+
           <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button">+ Novo horário</button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                { value: "0", label: "Domingo" },
+                { value: "1", label: "Segunda-Feira" },
+                { value: "2", label: "Terça-feira" },
+                { value: "3", label: "Quart-feira" },
+                { value: "4", label: "Quinta-feira" },
+                { value: "5", label: "Sexta-feira" },
+                { value: "6", label: "Sábado" },
+              ]}
+            />
+
+            <Input name="from" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
         </fieldset>
 
         <footer>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import PageHeader from "../../components/PageHeader";
 import TeacherItem from "../../components/TeacherItem";
 import Input from "../../components/Input";
+import Select from "../../components/Select";
 
 import "./styles.css";
 
@@ -11,8 +12,37 @@ const TeacherList = () => {
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis">
         <form id="search-teachers">
-          <Input name="subject" label="Matéria" />
-          <Input name="week_day" label="Dia da semana" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Ciências", label: "Ciências" },
+              { value: "Educação Física", label: "Educação Física" },
+              { value: "Física", label: "Física" },
+              { value: "Química", label: "Química" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "Historia", label: "Historia" },
+              { value: "Matemática", label: "Geografia" },
+              { value: "Português", label: "Geografia" },
+            ]}
+          />
+
+          <Select
+            name="week_day"
+            label="Dia da semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda-Feira" },
+              { value: "2", label: "Terça-feira" },
+              { value: "3", label: "Quart-feira" },
+              { value: "4", label: "Quinta-feira" },
+              { value: "5", label: "Sexta-feira" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
+
           <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
