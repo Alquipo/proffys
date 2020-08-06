be8c858e9052d0cb863b5d525e4e3485d186a930
Alquipo <alquiponeto@outlook.com.br>
2020-08-06 15:39:04 -0300

    Feat: Router Tabs Study page

---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const TeacherList = () => {
+  return (
+    <View>
+      <Text>sdsd</Text>
+    </View>
+  );
+};
+
+export default TeacherList;
